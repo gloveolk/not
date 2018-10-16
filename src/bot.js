e028f8745389f78d68edd2fa7a3ca06942911607
gloveolk <44202177+gloveolk@users.noreply.github.com>
2018-10-16 23:24:24 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,8 @@ let line = require('./main.js');
 let LINE = new line();
 
 const auth = {
-  authToken: 'HERE',
-  certificate: 'HERE'
+  authToken: 'WkT3FqeTyp8FP2FEuLU5iNCO4QOQ2dfMX9pe7jSEO8L2IE+NkZMx33L98u1ODyE73ocBlDoEjaUbxNcDfC6R6BCgzZJIlnARwPjanCkgLfExQm1RMIRKiS2B/f18/mjxoEl+LhiYYYwbVqi+28bkcQdB04t89/1O/w1cDnyilFU=',
+  certificate: '4675701c310c8736841def25dc0b0026'
 }
 //let client =  new LineConnect(auth);
    let client =  new LineConnect();
